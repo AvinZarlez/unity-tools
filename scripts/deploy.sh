7e4910d9a7a9a26ede4a831354111fde7d0bc305
Felix Rieseberg <felix@felixrieseberg.com>
2016-01-26 15:38:54 -0800

    :frog: If/Else Deploy Output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 
 if [[ "$TRAVIS_TAG" == "v"* ]]; then
     # Build only if there's a tag that starts with a v'
+    echo "Publishing using VSCE"
     npm install -g vsce
     vsce publish <<< $TOBIAH_KEY
+else
+    echo "Build not tagged, not deploying"
+    echo "Build tag :"
+    echo $TRAVIS_TAG
 fi
