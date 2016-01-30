26d90e19b84dbc27985a77c43d24a6e372464627
Katherine Harris <kvm.harris@gmail.com>
2016-01-30 10:30:40 -0800

    Geration of Org Directories complete

---
+++ src/directories.ts
@@ -0,0 +1,15 @@
+
+let fs = require('fs-extra');
+
+var folders: Array<string> = ['Scripts', 'Scenes', 'Prefabs', 'Materials', 'Textures'];
+
+export function GenerateOrganizationFolders(path: string)
+{
+    for(var j = 0; j <folders.length; j++){
+        var dir = path + folders[j];
+        fs.ensureDir(dir, function (err) {
+        console.log(err) // => null
+        // dir has now been created, including the directory it is to be placed in
+        });
+    }
+}
