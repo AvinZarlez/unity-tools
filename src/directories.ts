6878df740537ad7f1f54efca3deefbd672093738
Katherine Harris <kvm.harris@gmail.com>
2016-05-15 12:34:15 -0700

    editing fs-extra

--- src/directories.ts
+++ src/directories.ts
@@ -1,5 +1,5 @@
 
-let fs = require('fs-extra');
+let fs = require('mkdirs');
 
 var folders: Array<string> = ['Scripts', 'Scenes', 'Prefabs', 'Materials', 'Textures', 'Audio'];
 
@@ -7,9 +7,11 @@ export function GenerateOrganizationFolders(path: string)
 {
     for(var j = 0; j <folders.length; j++){
         var dir = path + folders[j];
-        fs.ensureDir(dir, function (err) {
+        fs.mkdirs(dir, function (err) {
         console.log(err)
         // dir has now been created, including the directory it is to be placed in
         });
     }
 }
+
+
