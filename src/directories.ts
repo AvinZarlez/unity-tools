2105ff67704faed9e054840f1f725d4fa640cc1c
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2016-09-26 15:51:16 -0700

    Made default folders actually work

--- src/directories.ts
+++ src/directories.ts
@@ -3,13 +3,8 @@ import path = require('path');
 
 let fs = require('fs');
 
-
-var folders: Array<string> = ["Scenes", "Prefabs", "Scripts", "Materials", "Audio"];
-
-export function GenerateOrganizationFolders(path: string)
+export function GenerateOrganizationFolders(path: string, generationFolders)
 {
-    var generationFolders;
-    generationFolders = setDefaultOrCustomFolders();
     for(var j = 0; j <generationFolders.length; j++){
         var dir = path + generationFolders[j];
         fs.mkdir(dir, function (err) {
@@ -19,34 +14,3 @@ export function GenerateOrganizationFolders(path: string)
         });       
     }
 }
-
-// Checks whether or not the User changed the default folder array
-export function setDefaultOrCustomFolders() : Array<String> {
-    if (!workspace || !workspace.rootPath) return null;
-    let fSettings = getDefaultFoldersFromSettings();
-    let fDefault = folders;
-    
-    // Quick sanity check
-    let userFolders = (fSettings && fSettings.length > 0 ) ? fSettings : fDefault;
-    return userFolders;
-}
-
-
-export function getDefaultFoldersFromSettings(): Array<String> {
-    if (!workspace || !workspace.rootPath) return null;
-
-    let settingsFile = path.join(workspace.rootPath,'.vscode', 'settings.json');
-    try {
-        let settings = JSON.parse(fs.readFileSync(settingsFile, 'utf8'));
-
-        if (settings) {
-            var usersDefaultFolders = settings['unity.setDefaultOrganizationFolders']; 
-            return usersDefaultFolders;
-        } else {
-            return folders;
-        }
-    } catch (e) {
-        return folders;
-    }
-}
-
--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as directories from './directories';
 
 let fs = require('fs');
 
-function openDocErrorMessage (str) {
+function openDocErrorMessage(str) {
 	return vscode.window.showErrorMessage("Error: " + str, "Open Docs").then((item) => {
 		if (item === "Open Docs") {
 			search.openURL();
@@ -15,19 +15,19 @@ function openDocErrorMessage (str) {
 export function activate(context: vscode.ExtensionContext) {
 
 	//Tell the user the extension has been activated.
-	console.log('Unity Tools extension is now active!'); 
+	console.log('Unity Tools extension is now active!');
 
 	// Open Unity Documentation, when you already have something you want to search selected
 	var open_docs = vscode.commands.registerTextEditorCommand("unity.OpenDocs",
 		(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-				
+
 			// selection[0] is the start, and selection[1] is the end
 			let selection = textEditor.selection;
 			if (!selection.isSingleLine) {
 				openDocErrorMessage("Multiple lines selected, please just select a class.");
 				return;
 			}
-			
+
 			let range = undefined;
 			if (!selection.isEmpty) {
 				// selection is not empty, get text from it
@@ -43,61 +43,51 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 
 			search.openUnityDocs(textEditor.document.lineAt(range.start.line).text, range.start.character, range.end.character);
-	});
+		});
 	context.subscriptions.push(open_docs);
-	
-	var search_docs = vscode.commands.registerCommand("unity.SearchDocs",()=>{
+
+	var search_docs = vscode.commands.registerCommand("unity.SearchDocs", () => {
 		vscode.window.showInputBox({
 			prompt: "Search the Unity Documentation:"
 		}).then((result) => {
-			if (result != undefined) {	
+			if (result != undefined) {
 				//Use the node module "open" to open a web browser
 				search.openURL(result);
 			}
 		});
 	});
 	context.subscriptions.push(search_docs);
-    
-	var open_vscode_docs = vscode.commands.registerCommand("unity.OpenVSCodeDocs",()=>{
+
+	var open_vscode_docs = vscode.commands.registerCommand("unity.OpenVSCodeDocs", () => {
 		// Using OpenURL from search to open VS Documentation.
         // Passing "true" to open the URL directly (instead of searching Unity docs)
         search.openURL("https://code.visualstudio.com/Docs/runtimes/unity", true);
-        
+
 	});
 	context.subscriptions.push(open_vscode_docs);
 
-	var get_assetstore_plugin = vscode.commands.registerCommand("unity.GetAssetStorePlugin",()=>{
+	var get_assetstore_plugin = vscode.commands.registerCommand("unity.GetAssetStorePlugin", () => {
 		search.openURL("http://u3d.as/jmM", true);
-        return vscode.window.showErrorMessage("Add to your Unity project, and remember check \"Enable Integration\"","How To","Git").then((item) => {
-		  if (item === "How To") {
-			// Using OpenURL from search to open Unity plug-in Documentation in git repo.
-            // Passing "true" to open the URL directly (instead of searching Unity docs)
-            search.openURL("https://github.com/dotBunny/VSCode/blob/master/HOWTO.pdf",true);
-		  } else if (item === "Git") {
-			// Using OpenURL from search to open Unity plug-in git repo.
-            // Passing "true" to open the URL directly (instead of searching Unity docs)
-            search.openURL("https://github.com/dotBunny/VSCode/",true);
-          }
-	    });
+        return vscode.window.showErrorMessage("Add to your Unity project, and remember check \"Enable Integration\"", "How To", "Git").then((item) => {
+			if (item === "How To") {
+				// Using OpenURL from search to open Unity plug-in Documentation in git repo.
+				// Passing "true" to open the URL directly (instead of searching Unity docs)
+				search.openURL("https://github.com/dotBunny/VSCode/blob/master/HOWTO.pdf", true);
+			} else if (item === "Git") {
+				// Using OpenURL from search to open Unity plug-in git repo.
+				// Passing "true" to open the URL directly (instead of searching Unity docs)
+				search.openURL("https://github.com/dotBunny/VSCode/", true);
+			}
+		});
 	});
 	context.subscriptions.push(get_assetstore_plugin);
-	
-    
-    var create_Directories = vscode.commands.registerCommand("unity.CreateDirectories", () => {
-		let settings : vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('unity');
 
-		var folderList = settings.get('defaultOrganizationFolders') || [
-			"Materials",
-			"Scenes",
-			"Scripts",
-			"Prefabs",
-			"Audio"
-        ];
-		vscode.window.showErrorMessage("R: "+folderList);
+
+    var create_Directories = vscode.commands.registerCommand("unity.CreateDirectories", () => {
 		var rootPath = vscode.workspace.rootPath;
-		if(rootPath != undefined){
+		if (rootPath != undefined) {
 			fs.stat(rootPath, (err, stats) => {
-				if(err && err.code === 'ENOENT'){
+				if (err && err.code === 'ENOENT') {
 					vscode.window.showErrorMessage("You do not have access or permission to this file on the hard drive.");
 				} else if (stats.isDirectory()) {
 					var rootPath = vscode.workspace.rootPath + '/Assets/';
@@ -110,18 +100,29 @@ export function activate(context: vscode.ExtensionContext) {
 							vscode.window.showErrorMessage("Something went wrong while checking Assets folder existence: " + err)
 						} else if (stats.isDirectory()) {
 							// Folder exists! Generate default folders. 
-							directories.GenerateOrganizationFolders(rootPath); 
+
+							var settings: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('unity');
+
+							var folderList = settings.get('defaultOrganizationFolders', [
+								"Materials",
+								"Scenes",
+								"Scripts",
+								"Prefabs",
+								"Audio"
+							]);
+
+							directories.GenerateOrganizationFolders(rootPath, folderList);
 							vscode.window.showInformationMessage("Folders generated sucessfully");
 						}
 					});
 				}
 			});
-		}else{
+		} else {
 			vscode.window.showErrorMessage("You do not have a workspace open in VSCode. Please 'Open Folder' to the root folder of a desired Unity Project.");
 		}
-		
-	});     
-    context.subscriptions.push(create_Directories);  
-	
-   
+
+	});
+    context.subscriptions.push(create_Directories);
+
+
 }
